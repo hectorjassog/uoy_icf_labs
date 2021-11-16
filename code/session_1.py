5bbc23167dd5ea5455ad6c2d64d7e81f173073f9
hectorjassog <hjassog@gmail.com>
2021-11-16 17:07:17 +0000

    MSE for the super gaussian working

--- code/session_1.py
+++ code/session_1.py
@@ -4,7 +4,6 @@ from scipy.optimize import curve_fit
 import numpy as np
 
 def gaussian(x, *params):
-    print (params)
 
     A = params[0]
     x0 = params[1]
@@ -13,42 +12,77 @@ def gaussian(x, *params):
 
     return y0 + A*np.exp(-(x-x0)**2 / (2*c*c))
 
+def super_gaussian(x, *params):
+    if n == 0:
+        print ("n not set")
+    A = params[0]
+    x0 = params[1]
+    c = params[2]
+    y0 = params[3]
+
+    return y0 + A*np.exp(-((x-x0)/(np.sqrt(2)*c))**n)
+
+n = 0
+ns = [2, 4, 6, 8]
+guess = [5000, 1, 0.5, 28000]
+print ("Our initial guess is: {0}".format(guess))
 file_names = ["vertical", "horizontal", "diagonal_top_left_to_bottom_right", "diagonal_top_right_to_bottom_left"]
+for n_value in ns:
+    n = n_value
 
-for file_name in file_names:
-    # xdata, ydata = icf.load_2col("../data/session1/{0}.csv".format(file_name))
-    xdata, ydata = icf.load_2col("../data/session1/{0}.csv".format(file_name))
-
-    xdata *= 60.0/1000.0
-
-
-    if file_name == "diagonal_top_left_to_bottom_right":
-        # xdata = xdata[:]
-        pass
-
-    # plt.plot(xdata, ydata)
-    # plt.title("Lineout: {}".format(file_name))
-    # plt.xlabel("Position - mm")
-    # plt.ylabel("Brightness units")
-    # plt.show()
+    mse_values = []
+    mse_values_sg = []
 
 
+    for file_name in file_names:
+        if file_name == "vertical":
+            # xdata, ydata = icf.load_2col("../data/session1/{0}.csv".format(file_name))
+            xdata, ydata = icf.load_2col("../data/session1/{0}.csv".format(file_name))
+
+            xdata *= 60.0/1000.0
+
+            # plt.plot(xdata, ydata)
+            # plt.title("Lineout: {}".format(file_name))
+            # plt.xlabel("Position - mm")
+            # plt.ylabel("Brightness units")
+            # plt.show()
 
 
-    guess = [5000, 1, 1, 28000]
+            popt, pcov = curve_fit(gaussian, xdata, ydata, p0=guess)
+            popt_sg, pcov_sg = curve_fit(super_gaussian, xdata, ydata, p0=guess)
 
-    print ("Our initial guess is: {0}".format(guess))
+            # for i in range(len(popt)):
+            #     print ("Parameter {0}: {1} +/- {2}".format(i, popt[i], np.sqrt(pcov[i][i])))
+
+            # print ("Fit parameters gaussian: {0}".format(popt))
+            # print ("Fit standard deviations gaussian: {0}".format(np.sqrt(np.diag(pcov))))
+
+            # for i in range(len(popt_sg)):
+            #     print ("Parameter {0}: {1} +/- {2}".format(i, popt_sg[i], np.sqrt(pcov_sg[i][i])))
+
+            # print ("Fit parameters super gaussian: {0}".format(popt_sg))
+            # print ("Fit standard deviations super gaussian: {0}".format(np.sqrt(np.diag(pcov_sg))))
+
+            yfit = gaussian(xdata, *popt)
+            yfit_sg = super_gaussian(xdata, *popt_sg)
+
+            mse = np.mean((ydata - yfit)**2)
+            mse_values.append(mse)
+            mse_sg = np.mean((ydata - yfit_sg)**2)
+            mse_values_sg.append(mse_sg)
 
 
-    popt, pcov = curve_fit(gaussian, xdata, ydata, p0=guess)
+            # print ("MSE for super gaussian {} with n {}")
 
-    for i in range(len(popt)):
-        print ("Parameter {0}: {1} +/- {2}".format(i, popt[i], np.sqrt(pcov[i][i])))
-
-    print ("Fit parameters : {0}".format(popt))
-    print ("Fit standard deviations : {0}".format(np.sqrt(np.diag(pcov))))
-
-    yfit = gaussian(xdata, *popt)
-
-    icf.fit_plot(xdata, ydata, yfit)
+            plt.plot(xdata, ydata, label="data")
+            plt.plot(xdata, yfit, label="gaussian")
+            plt.plot(xdata, yfit_sg, label="super gaussian")
+            plt.legend(loc="best")
+            plt.show()
+        else: 
+            pass
+    
+    print ("For the super gaussian with n = {0}, the MSE = {1}".format(n_value, np.mean(mse_values_sg)))
+    # break
+    # icf.fit_plot(xdata, ydata, yfit)
 
