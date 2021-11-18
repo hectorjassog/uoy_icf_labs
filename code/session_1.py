161d3634b212bf16d9130a51a6d0fc4168a69d61
hectorjassog <hjassog@gmail.com>
2021-11-18 10:22:03 +0000

    n value found

--- code/session_1.py
+++ code/session_1.py
@@ -10,7 +10,7 @@ def gaussian(x, *params):
     c = params[2]
     y0 = params[3]
 
-    return y0 + A*np.exp(-(x-x0)**2 / (2*c*c))
+    return y0 + A*np.exp(-(x-x0)**2 / (2*c**2))
 
 def super_gaussian(x, *params):
     if n == 0:
@@ -23,20 +23,23 @@ def super_gaussian(x, *params):
     return y0 + A*np.exp(-((x-x0)/(np.sqrt(2)*c))**n)
 
 n = 0
-ns = [2, 4, 6, 8]
+ns = [4, 6]
 guess = [5000, 1, 0.5, 28000]
 print ("Our initial guess is: {0}".format(guess))
 file_names = ["vertical", "horizontal", "diagonal_top_left_to_bottom_right", "diagonal_top_right_to_bottom_left"]
-for n_value in ns:
+mse_values = []
+calculated_mse_values_sg = []
+# matrix to store the values to be plotted
+mse_for_n_file_name = np.zeros((len(ns), len(file_names)))
+for idxn, n_value in enumerate(ns):
     n = n_value
 
-    mse_values = []
     mse_values_sg = []
 
 
-    for file_name in file_names:
-        if file_name == "vertical":
-            # xdata, ydata = icf.load_2col("../data/session1/{0}.csv".format(file_name))
+    for idxf, file_name in enumerate(file_names):
+        #if file_name == "horizontal":
+        if 1==1:    
             xdata, ydata = icf.load_2col("../data/session1/{0}.csv".format(file_name))
 
             xdata *= 60.0/1000.0
@@ -66,23 +69,37 @@ for n_value in ns:
             yfit = gaussian(xdata, *popt)
             yfit_sg = super_gaussian(xdata, *popt_sg)
 
+            # the MSE for the gaussian doesn't really change with respect to N, but it is easier to have it in the forloop. 
+            # we will just add the same values several times and get the mean, so it won't affect the result.
             mse = np.mean((ydata - yfit)**2)
             mse_values.append(mse)
             mse_sg = np.mean((ydata - yfit_sg)**2)
             mse_values_sg.append(mse_sg)
 
 
-            # print ("MSE for super gaussian {} with n {}")
+            print ("For super gaussian with n {0} and for file {1} is: {2}".format(n, file_name, mse_sg))
+            mse_for_n_file_name[idxn][idxf] = mse_sg
 
-            plt.plot(xdata, ydata, label="data")
-            plt.plot(xdata, yfit, label="gaussian")
-            plt.plot(xdata, yfit_sg, label="super gaussian")
-            plt.legend(loc="best")
-            plt.show()
+            # plt.plot(n_value, mse_values_sg[0], label=str(file_name))
+            # print (file_name)
+            # print (mse_values_sg[0])
+
+            # plt.plot(xdata, ydata, label="data")
+            # plt.plot(xdata, yfit, label="gaussian")
+            # plt.plot(xdata, yfit_sg, label="super gaussian")
+            # plt.legend(loc="best")
+            # plt.show()
         else: 
             pass
-    
-    print ("For the super gaussian with n = {0}, the MSE = {1}".format(n_value, np.mean(mse_values_sg)))
-    # break
-    # icf.fit_plot(xdata, ydata, yfit)
+    n_mse_sg = np.mean(mse_values_sg)
+    print ("For the super gaussian with n = {0}, the average MSE = {1}".format(n_value, n_mse_sg))
+    calculated_mse_values_sg.append(n_mse_sg)
+
+print ("For the gaussian, the MSE = {1}".format(n_value, np.mean(mse_values)))
+
+plt.plot(ns, mse_for_n_file_name)
+plt.xlabel("Values of n")
+plt.ylabel("Mean Squared Error (MSE)")
+plt.show()
+# icf.fit_plot(xdata, ydata, yfit)
 
